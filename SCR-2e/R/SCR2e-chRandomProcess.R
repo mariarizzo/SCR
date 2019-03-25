d5bbf15ab9adb428dfaa3a6f40d9195a679d8765
Maria Rizzo <mrizzo@admin-172-18-77-168.wireless.bgsu.edu>
2019-03-25 12:41:57 -0400

    info

--- SCR-2e/R/SCR2e-chRandomProcess.R
+++ SCR-2e/R/SCR2e-chRandomProcess.R
@@ -3,7 +3,7 @@
 ###       Maria L. Rizzo                            ###
 ###       Chapman & Hall/CRC The R Series           ###
 ###       ISBN 9781466553323 - CAT# K15269          ###
-###       January 2019                              ###
+###       March 6, 2019                             ###
 ###                                                 ###
 ###       R code for Chapter 4                      ###
 ###       Generating Random Processes               ###
